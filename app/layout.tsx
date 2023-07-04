6f6c6454bad71e4cedd80103a24709cac9f64c53
Henrique Macedo <hey@ickas.dev>
2023-07-04 19:13:00 +0100

    Setup global styling and tokens

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,7 @@
+import { Inter } from "next/font/google";
 import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
@@ -9,7 +12,7 @@ export default function RootLayout({
     <>
       <html lang="en">
         <head />
-        <body>{children}</body>
+        <body className={inter.className}>{children}</body>
       </html>
     </>
   );
