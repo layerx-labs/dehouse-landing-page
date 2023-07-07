9b3faef563f90cb605df59f20e84e08ff0e68d77
Henrique Macedo <hey@ickas.dev>
2023-07-07 13:19:32 +0100

    Fix SVG attribute names

--- app/components/ui/icons/services.tsx
+++ app/components/ui/icons/services.tsx
@@ -7,27 +7,27 @@ export function Assistance() {
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <g clip-path="url(#clip0_81_59)">
+      <g clipPath="url(#clip0_81_59)">
         <path
           d="M6 26H2C1.73478 26 1.48043 25.8946 1.29289 25.7071C1.10536 25.5196 1 25.2652 1 25V20C1 19.7348 1.10536 19.4804 1.29289 19.2929C1.48043 19.1054 1.73478 19 2 19H6"
           stroke="url(#paint0_linear_81_59)"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M14 20H18L26.375 18.0738C26.6829 17.9894 27.0061 17.9771 27.3195 18.038C27.6329 18.0989 27.9281 18.2312 28.182 18.4247C28.436 18.6181 28.6419 18.8676 28.7838 19.1536C28.9256 19.4396 28.9996 19.7545 29 20.0738C29.0002 20.4592 28.8929 20.8371 28.6902 21.165C28.4875 21.4929 28.1974 21.7578 27.8525 21.93L23 24L15 26H6V19L9.125 15.875C9.40432 15.5967 9.73577 15.3761 10.1004 15.226C10.465 15.0758 10.8557 14.999 11.25 15H17.5C18.163 15 18.7989 15.2634 19.2678 15.7322C19.7366 16.2011 20 16.837 20 17.5C20 18.1631 19.7366 18.7989 19.2678 19.2678C18.7989 19.7366 18.163 20 17.5 20H14Z"
           stroke="url(#paint1_linear_81_59)"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M12.0913 15C10.875 13.465 10 11.82 10 9.99997C10 7.28872 12.2087 4.99997 14.9325 4.99997C15.906 4.98855 16.8607 5.26796 17.6744 5.80242C18.4881 6.33688 19.1238 7.10206 19.5 7.99997C19.8762 7.10206 20.5119 6.33688 21.3256 5.80242C22.1393 5.26796 23.094 4.98855 24.0675 4.99997C26.7912 4.99997 29 7.28872 29 9.99997C29 13.6537 25.4775 16.8837 22.7225 18.915"
           stroke="url(#paint2_linear_81_59)"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
       </g>
       <defs>
@@ -39,8 +39,8 @@ export function Assistance() {
           y2="26"
           gradientUnits="userSpaceOnUse"
         >
-          <stop stop-color="#9D5CF4" />
-          <stop offset="1" stop-color="#9D5CF4" stop-opacity="0.51" />
+          <stop stopColor="#9D5CF4" />
+          <stop offset="1" stopColor="#9D5CF4" stopOpacity="0.51" />
         </linearGradient>
         <linearGradient
           id="paint1_linear_81_59"
@@ -50,8 +50,8 @@ export function Assistance() {
           y2="26"
           gradientUnits="userSpaceOnUse"
         >
-          <stop stop-color="#9D5CF4" />
-          <stop offset="1" stop-color="#9D5CF4" stop-opacity="0.51" />
+          <stop stopColor="#9D5CF4" />
+          <stop offset="1" stopColor="#9D5CF4" stopOpacity="0.51" />
         </linearGradient>
         <linearGradient
           id="paint2_linear_81_59"
@@ -61,8 +61,8 @@ export function Assistance() {
           y2="18.915"
           gradientUnits="userSpaceOnUse"
         >
-          <stop stop-color="#9D5CF4" />
-          <stop offset="1" stop-color="#9D5CF4" stop-opacity="0.51" />
+          <stop stopColor="#9D5CF4" />
+          <stop offset="1" stopColor="#9D5CF4" stopOpacity="0.51" />
         </linearGradient>
         <clipPath id="clip0_81_59">
           <rect width="32" height="32" fill="white" />
@@ -81,34 +81,34 @@ export function Forniture() {
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <g clip-path="url(#clip0_81_67)">
+      <g clipPath="url(#clip0_81_67)">
         <path
           d="M9.33334 29C9.33334 28.2044 9.64941 27.4413 10.212 26.8787C10.7746 26.3161 11.5377 26 12.3333 26H20.3333C21.129 26 21.8921 26.3161 22.4547 26.8787C23.0173 27.4413 23.3333 28.2044 23.3333 29"
           stroke="url(#paint0_linear_81_67)"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M16.3333 21V29"
           stroke="url(#paint1_linear_81_67)"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M2.33334 16H5.33334C5.33334 17.3261 5.86013 18.5979 6.79781 19.5355C7.73549 20.4732 9.00726 21 10.3333 21H22.3333C23.6594 21 24.9312 20.4732 25.8689 19.5355C26.8066 18.5979 27.3333 17.3261 27.3333 16H30.3333"
           stroke="url(#paint2_linear_81_67)"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M12.0483 3C11.8076 2.99999 11.5749 3.08684 11.3931 3.24459C11.2112 3.40235 11.0923 3.62041 11.0583 3.85875L9.34334 15.8588C9.3231 16.0006 9.33358 16.1451 9.37405 16.2825C9.41452 16.4199 9.48405 16.5471 9.57792 16.6553C9.6718 16.7635 9.78783 16.8503 9.91816 16.9098C10.0485 16.9692 10.1901 17 10.3333 17H22.3333C22.4766 17 22.6182 16.9692 22.7485 16.9098C22.8788 16.8503 22.9949 16.7635 23.0888 16.6553C23.1826 16.5471 23.2522 16.4199 23.2926 16.2825C23.3331 16.1451 23.3436 16.0006 23.3233 15.8588L21.6083 3.85875C21.5743 3.62041 21.4555 3.40235 21.2736 3.24459C21.0918 3.08684 20.8591 2.99999 20.6183 3H12.0483Z"
           stroke="url(#paint3_linear_81_67)"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
       </g>
       <defs>
@@ -120,8 +120,8 @@ export function Forniture() {
           y2="29"
           gradientUnits="userSpaceOnUse"
         >
-          <stop stop-color="#9D5CF4" />
-          <stop offset="1" stop-color="#9D5CF4" stop-opacity="0.51" />
+          <stop stopColor="#9D5CF4" />
+          <stop offset="1" stopColor="#9D5CF4" stopOpacity="0.51" />
         </linearGradient>
         <linearGradient
           id="paint1_linear_81_67"
@@ -131,8 +131,8 @@ export function Forniture() {
           y2="29"
           gradientUnits="userSpaceOnUse"
         >
-          <stop stop-color="#9D5CF4" />
-          <stop offset="1" stop-color="#9D5CF4" stop-opacity="0.51" />
+          <stop stopColor="#9D5CF4" />
+          <stop offset="1" stopColor="#9D5CF4" stopOpacity="0.51" />
         </linearGradient>
         <linearGradient
           id="paint2_linear_81_67"
@@ -142,8 +142,8 @@ export function Forniture() {
           y2="21"
           gradientUnits="userSpaceOnUse"
         >
-          <stop stop-color="#9D5CF4" />
-          <stop offset="1" stop-color="#9D5CF4" stop-opacity="0.51" />
+          <stop stopColor="#9D5CF4" />
+          <stop offset="1" stopColor="#9D5CF4" stopOpacity="0.51" />
         </linearGradient>
         <linearGradient
           id="paint3_linear_81_67"
@@ -153,8 +153,8 @@ export function Forniture() {
           y2="17"
           gradientUnits="userSpaceOnUse"
         >
-          <stop stop-color="#9D5CF4" />
-          <stop offset="1" stop-color="#9D5CF4" stop-opacity="0.51" />
+          <stop stopColor="#9D5CF4" />
+          <stop offset="1" stopColor="#9D5CF4" stopOpacity="0.51" />
         </linearGradient>
         <clipPath id="clip0_81_67">
           <rect
@@ -178,34 +178,34 @@ export function Rooms() {
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <g clip-path="url(#clip0_81_76)">
+      <g clipPath="url(#clip0_81_76)">
         <path
           d="M27.6667 6H5.66669C5.1144 6 4.66669 6.44772 4.66669 7V22C4.66669 22.5523 5.1144 23 5.66669 23H27.6667C28.219 23 28.6667 22.5523 28.6667 22V7C28.6667 6.44772 28.219 6 27.6667 6Z"
           stroke="url(#paint0_linear_81_76)"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M20.6667 23L24.6667 28"
           stroke="url(#paint1_linear_81_76)"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M12.6667 23L8.66669 28"
           stroke="url(#paint2_linear_81_76)"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M16.6667 6V3"
           stroke="url(#paint3_linear_81_76)"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
       </g>
       <defs>
@@ -217,8 +217,8 @@ export function Rooms() {
           y2="23"
           gradientUnits="userSpaceOnUse"
         >
-          <stop stop-color="#9D5CF4" />
-          <stop offset="1" stop-color="#9D5CF4" stop-opacity="0.51" />
+          <stop stopColor="#9D5CF4" />
+          <stop offset="1" stopColor="#9D5CF4" stopOpacity="0.51" />
         </linearGradient>
         <linearGradient
           id="paint1_linear_81_76"
@@ -228,8 +228,8 @@ export function Rooms() {
           y2="28"
           gradientUnits="userSpaceOnUse"
         >
-          <stop stop-color="#9D5CF4" />
-          <stop offset="1" stop-color="#9D5CF4" stop-opacity="0.51" />
+          <stop stopColor="#9D5CF4" />
+          <stop offset="1" stopColor="#9D5CF4" stopOpacity="0.51" />
         </linearGradient>
         <linearGradient
           id="paint2_linear_81_76"
@@ -239,8 +239,8 @@ export function Rooms() {
           y2="28"
           gradientUnits="userSpaceOnUse"
         >
-          <stop stop-color="#9D5CF4" />
-          <stop offset="1" stop-color="#9D5CF4" stop-opacity="0.51" />
+          <stop stopColor="#9D5CF4" />
+          <stop offset="1" stopColor="#9D5CF4" stopOpacity="0.51" />
         </linearGradient>
         <linearGradient
           id="paint3_linear_81_76"
@@ -250,8 +250,8 @@ export function Rooms() {
           y2="6"
           gradientUnits="userSpaceOnUse"
         >
-          <stop stop-color="#9D5CF4" />
-          <stop offset="1" stop-color="#9D5CF4" stop-opacity="0.51" />
+          <stop stopColor="#9D5CF4" />
+          <stop offset="1" stopColor="#9D5CF4" stopOpacity="0.51" />
         </linearGradient>
         <clipPath id="clip0_81_76">
           <rect
@@ -275,20 +275,20 @@ export function Time() {
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <g clip-path="url(#clip0_81_86)">
+      <g clipPath="url(#clip0_81_86)">
         <path
           d="M16 28C22.6274 28 28 22.6274 28 16C28 9.37258 22.6274 4 16 4C9.37258 4 4 9.37258 4 16C4 22.6274 9.37258 28 16 28Z"
           stroke="url(#paint0_linear_81_86)"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M16 9V16H23"
           stroke="url(#paint1_linear_81_86)"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
       </g>
       <defs>
@@ -300,8 +300,8 @@ export function Time() {
           y2="28"
           gradientUnits="userSpaceOnUse"
         >
-          <stop stop-color="#9D5CF4" />
-          <stop offset="1" stop-color="#9D5CF4" stop-opacity="0.51" />
+          <stop stopColor="#9D5CF4" />
+          <stop offset="1" stopColor="#9D5CF4" stopOpacity="0.51" />
         </linearGradient>
         <linearGradient
           id="paint1_linear_81_86"
@@ -311,8 +311,8 @@ export function Time() {
           y2="16"
           gradientUnits="userSpaceOnUse"
         >
-          <stop stop-color="#9D5CF4" />
-          <stop offset="1" stop-color="#9D5CF4" stop-opacity="0.51" />
+          <stop stopColor="#9D5CF4" />
+          <stop offset="1" stopColor="#9D5CF4" stopOpacity="0.51" />
         </linearGradient>
         <clipPath id="clip0_81_86">
           <rect width="32" height="32" fill="white" />
@@ -331,27 +331,27 @@ export function Access() {
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <g clip-path="url(#clip0_81_93)">
+      <g clipPath="url(#clip0_81_93)">
         <path
           d="M3.33334 28H29.3333"
           stroke="url(#paint0_linear_81_93)"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M7.33334 28V5C7.33334 4.73478 7.4387 4.48043 7.62624 4.29289C7.81377 4.10536 8.06813 4 8.33334 4H24.3333C24.5986 4 24.8529 4.10536 25.0405 4.29289C25.228 4.48043 25.3333 4.73478 25.3333 5V28"
           stroke="url(#paint1_linear_81_93)"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M20.3333 4C20.5986 4 20.8529 4.10536 21.0405 4.29289C21.228 4.48043 21.3333 4.73478 21.3333 5V28"
           stroke="url(#paint2_linear_81_93)"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M16.8333 18C17.6618 18 18.3333 17.3284 18.3333 16.5C18.3333 15.6716 17.6618 15 16.8333 15C16.0049 15 15.3333 15.6716 15.3333 16.5C15.3333 17.3284 16.0049 18 16.8333 18Z"
@@ -367,8 +367,8 @@ export function Access() {
           y2="29"
           gradientUnits="userSpaceOnUse"
         >
-          <stop stop-color="#9D5CF4" />
-          <stop offset="1" stop-color="#9D5CF4" stop-opacity="0.51" />
+          <stop stopColor="#9D5CF4" />
+          <stop offset="1" stopColor="#9D5CF4" stopOpacity="0.51" />
         </linearGradient>
         <linearGradient
           id="paint1_linear_81_93"
@@ -378,8 +378,8 @@ export function Access() {
           y2="28"
           gradientUnits="userSpaceOnUse"
         >
-          <stop stop-color="#9D5CF4" />
-          <stop offset="1" stop-color="#9D5CF4" stop-opacity="0.51" />
+          <stop stopColor="#9D5CF4" />
+          <stop offset="1" stopColor="#9D5CF4" stopOpacity="0.51" />
         </linearGradient>
         <linearGradient
           id="paint2_linear_81_93"
@@ -389,8 +389,8 @@ export function Access() {
           y2="28"
           gradientUnits="userSpaceOnUse"
         >
-          <stop stop-color="#9D5CF4" />
-          <stop offset="1" stop-color="#9D5CF4" stop-opacity="0.51" />
+          <stop stopColor="#9D5CF4" />
+          <stop offset="1" stopColor="#9D5CF4" stopOpacity="0.51" />
         </linearGradient>
         <clipPath id="clip0_81_93">
           <rect
@@ -414,34 +414,34 @@ export function Expenses() {
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <g clip-path="url(#clip0_81_102)">
+      <g clipPath="url(#clip0_81_102)">
         <path
           d="M28.6667 7H4.66669C4.1144 7 3.66669 7.44772 3.66669 8V24C3.66669 24.5523 4.1144 25 4.66669 25H28.6667C29.219 25 29.6667 24.5523 29.6667 24V8C29.6667 7.44772 29.219 7 28.6667 7Z"
           stroke="url(#paint0_linear_81_102)"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M21.6667 21H25.6667"
           stroke="url(#paint1_linear_81_102)"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M15.6667 21H17.6667"
           stroke="url(#paint2_linear_81_102)"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M3.66669 12H29.6667"
           stroke="url(#paint3_linear_81_102)"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
       </g>
       <defs>
@@ -453,8 +453,8 @@ export function Expenses() {
           y2="25"
           gradientUnits="userSpaceOnUse"
         >
-          <stop stop-color="#9D5CF4" />
-          <stop offset="1" stop-color="#9D5CF4" stop-opacity="0.51" />
+          <stop stopColor="#9D5CF4" />
+          <stop offset="1" stopColor="#9D5CF4" stopOpacity="0.51" />
         </linearGradient>
         <linearGradient
           id="paint1_linear_81_102"
@@ -464,8 +464,8 @@ export function Expenses() {
           y2="22"
           gradientUnits="userSpaceOnUse"
         >
-          <stop stop-color="#9D5CF4" />
-          <stop offset="1" stop-color="#9D5CF4" stop-opacity="0.51" />
+          <stop stopColor="#9D5CF4" />
+          <stop offset="1" stopColor="#9D5CF4" stopOpacity="0.51" />
         </linearGradient>
         <linearGradient
           id="paint2_linear_81_102"
@@ -475,8 +475,8 @@ export function Expenses() {
           y2="22"
           gradientUnits="userSpaceOnUse"
         >
-          <stop stop-color="#9D5CF4" />
-          <stop offset="1" stop-color="#9D5CF4" stop-opacity="0.51" />
+          <stop stopColor="#9D5CF4" />
+          <stop offset="1" stopColor="#9D5CF4" stopOpacity="0.51" />
         </linearGradient>
         <linearGradient
           id="paint3_linear_81_102"
@@ -486,8 +486,8 @@ export function Expenses() {
           y2="13"
           gradientUnits="userSpaceOnUse"
         >
-          <stop stop-color="#9D5CF4" />
-          <stop offset="1" stop-color="#9D5CF4" stop-opacity="0.51" />
+          <stop stopColor="#9D5CF4" />
+          <stop offset="1" stopColor="#9D5CF4" stopOpacity="0.51" />
         </linearGradient>
         <clipPath id="clip0_81_102">
           <rect
