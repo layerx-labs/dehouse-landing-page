58bc48396b92dce4eee3ccc9c4407a1f07bbf108
Henrique Macedo <hey@ickas.dev>
2023-07-07 13:55:16 +0100

    Add panel component

---
+++ app/components/ui/panel/index.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import ButtonLink from "@/ui/button-link";
+import styles from "./styles.module.css";
+import { PanelProps } from "./types";
+
+export default function Panel(props: PanelProps) {
+  const { title, cover, children, buttonUrl, buttonLabel } = props;
+  const { width, height, url } = cover;
+
+  return (
+    <div className={styles.panel}>
+      <div className={styles.cover}>
+        <Image width={width} height={height} src={url} alt={title} />
+      </div>
+      <div className={styles.details}>
+        <h3>{title}</h3>
+        {children}
+      </div>
+      <ButtonLink url={buttonUrl} label={buttonLabel} icon="arrowRight" />
+    </div>
+  );
+}
---
+++ app/components/ui/panel/types.ts
@@ -0,0 +1,15 @@
+import { StaticImport } from "next/dist/shared/lib/get-img-props";
+
+export interface Cover {
+  url: string | StaticImport;
+  width: number;
+  height: number;
+}
+
+export interface PanelProps {
+  title: string;
+  cover: Cover;
+  children: React.ReactNode;
+  buttonUrl: string;
+  buttonLabel: string;
+}
