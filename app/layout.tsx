65ceed4a9f83676afaa445000562d50c237ba4eb
Henrique Macedo <hey@ickas.dev>
2023-07-08 10:20:23 +0100

    Improve SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,47 @@ import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
+const title = "DeHouse";
+const description = "The first web3 hub in Portugal";
+const og = "/dehouse-og.jpg";
+
+export const metadata = {
+  title: title,
+  description: description,
+  keywords: "web3, crypto, hub, cowork, cowork space, hacker space",
+  openGraph: {
+    url: "https://dehouse.org",
+    title: title,
+    description: description,
+    type: "website",
+    locale: "en-US",
+    images: [
+      {
+        url: og,
+        width: 1280,
+        height: 720,
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: title,
+    description: description,
+    creator: "@DeHouseDAO",
+    site: "@DeHouseDAO",
+    images: [og],
+  },
+  icons: {
+    icon: [{ url: "/favicon.ico" }, { url: "/icon.svg" }],
+    apple: "/apple-touch-icon.png",
+  },
+  robots: {
+    index: true,
+  },
+  authors: [{ name: "LayerX", url: "https://layerx.xyz" }],
+  manifest: "/manifest.webmanifest",
+};
+
 export default function RootLayout({
   children,
 }: {
@@ -11,7 +52,7 @@ export default function RootLayout({
   return (
     <>
       <html lang="en">
-        <head />
+        {/* <head /> */}
         <body className={inter.className}>{children}</body>
       </html>
     </>
