f2a0a2b5cc03882b78e1b5a5494c30205ac4ea95
Henrique Macedo <hey@ickas.dev>
2023-07-13 15:25:40 +0100

    Add remote pattern

--- next.config.js
+++ next.config.js
@@ -10,6 +10,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "dehouse.org",
       },
+      {
+        protocol: "https",
+        hostname: "api.dehouse.org",
+      },
     ],
   },
 };
